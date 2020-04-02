beda5378623407480f7232eefd5535f279143461
Jimi <jimi.gantinis@gmail.com>
2020-04-02 20:33:51 -0300

    Monster AI + Kill Player

---
+++ Assets/Scripts/damagePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damagePlayer : MonoBehaviour
+{
+    public int playerHealth = 30;
+    int damage = 1;
+
+    void Start()
+    {
+        print(playerHealth);
+    }
+
+    void OnCollisionEnter(Collision _collision)
+    {
+        if (_collision.gameObject.tag == "enemyDong")
+        {
+            playerHealth -= damage;
+            print("A escuridão está de dominando" + playerHealth);
+        }
+        if (playerHealth <= 0)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
